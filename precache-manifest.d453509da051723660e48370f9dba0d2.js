4fb1523c9ed52c0b2c6262d7e861338807b7a101
DESKTOP-FDSVPNE\Usuario <you@example.com>
2020-04-06 18:57:42 -0300

    Updates

---
+++ precache-manifest.d453509da051723660e48370f9dba0d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6daec56cb8fa13c9de7778fc345484f7",
+    "url": "/my-spotify/index.html"
+  },
+  {
+    "revision": "860d48e07caf54fa1b5a",
+    "url": "/my-spotify/static/css/main.7635d07d.chunk.css"
+  },
+  {
+    "revision": "d8f73cd9c7b031ff2974",
+    "url": "/my-spotify/static/js/2.4642b227.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-spotify/static/js/2.4642b227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860d48e07caf54fa1b5a",
+    "url": "/my-spotify/static/js/main.5e4a19ad.chunk.js"
+  },
+  {
+    "revision": "5ac2762cbf4d0501571e",
+    "url": "/my-spotify/static/js/runtime-main.52c2b2ef.js"
+  },
+  {
+    "revision": "2fb5826db22841857db3f086bb9e89f0",
+    "url": "/my-spotify/static/media/track.2fb5826d.jpg"
+  }
+]);
